9e78d1617809c53c954c5bc284643d5d74909d5e
guilhermepllay157@gmail.com <guilhermepllay157@gmail.com>
2020-10-20 21:33:30 -0300

    'First'

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://shopdin.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://shopdin.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/home/abcmarke/shopdin.com.br/catalog/');
+define('DIR_SYSTEM', '/home/abcmarke/shopdin.com.br/system/');
+define('DIR_IMAGE', '/home/abcmarke/shopdin.com.br/image/');
+define('DIR_STORAGE', '/home/abcmarke/public_html/shopdin/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'abcmarke_ocar296');
+define('DB_PASSWORD', ']pw69S[0A7');
+define('DB_DATABASE', 'abcmarke_ocar296');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocmw_');
+?>
